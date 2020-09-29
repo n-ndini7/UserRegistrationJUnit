632def76a78682c06f40518ceffb19b02dcc4058
n-ndini7 <hp@RJs-PC>
2020-09-30 00:32:39 +0530

    [nandini] added Uc1 - first name check

--- src/main/java/com/capgemini/UserRegGradle.java
+++ src/main/java/com/capgemini/UserRegGradle.java
@@ -1,8 +1,37 @@
 package com.capgemini;
 
-//Master branch - welcome message
+import java.util.regex.*;
+import java.util.*;
+
+//User registration first name check
 public class UserRegGradle {
+
+	public static Scanner sc;
+	public static String fname;
+
+	public void FnameCheck(String fname) {
+
+		Pattern p = Pattern.compile("^[A-Z][a-zA-Z]{2,}");
+		Matcher m = p.matcher(fname);
+		boolean flag = true;
+		while (flag) {
+			if (m.matches()) {
+				System.out.println("Entered First name is valid!!");
+				flag = false;
+				break;
+			} else {
+				System.out.println("Entered first name is invalid!! ");
+				break;
+			}
+		}
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to User Registration credentials validation program!");
+		sc = new Scanner(System.in);
+		UserRegGradleuser = new UserRegGradle();
+		System.out.println("Enter First name:");
+		fname = sc.nextLine();
+		user.FnameCheck(fname);
+
 	}
 }
