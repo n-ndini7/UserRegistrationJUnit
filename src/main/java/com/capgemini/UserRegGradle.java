c5b7650620c7c53fb62aa4a2434b98217b092e4a
n-ndini7 <hp@RJs-PC>
2020-09-30 00:38:46 +0530

    [nandini] added UC4 - mobile check

--- src/main/java/com/capgemini/UserRegGradle.java
+++ src/main/java/com/capgemini/UserRegGradle.java
@@ -1,37 +1,38 @@
 package com.capgemini;
+
 import java.util.regex.*;
 import java.util.*;
 
-//Email ID uc5 check for optional closing part 2
+//User registration mobile no. check.
 public class UserRegGradle {
 
-	public static String email;
 	public static Scanner sc;
-	public static void EmailCheck(String email) {
+	public static String mobile;
+
+	public void MobileCheck(String mobile) {
+
+		Pattern p = Pattern.compile("[0-9]{2}[\\s][7-9][0-9]{9}");
+		Matcher m = p.matcher(mobile);
 		boolean flag = true;
-		while(flag) {
-		Pattern p = Pattern.compile("^([a-z]){1,}([.+_-]){0,1}[0-9a-z]*(@){1}([0-9a-z]*)(.([a-z]){2,}){1}(.[a-z][a-z])?$");
-		Matcher m = p.matcher(email);
-		if(m.matches()) {
-			System.out.println("Email id is valid!!");
-			flag = false;
-			break;
-		}
-		else
-		{
-			System.out.println("Email id invalid!! Enter another mail id:");
-			email=sc.nextLine();
-			
+		while (flag) {
+			if (m.matches()) {
+				System.out.println("Entered mobile no. is valid!!");
+				flag = false;
+				break;
+			} else {
+				System.out.println("Entered mobile no. is invalid!! Re-enter mobile no.:");
+				mobile = sc.nextLine();
+				flag = true;
+			}
 		}
 	}
-		
-	}
+
 	public static void main(String[] args) {
 		sc = new Scanner(System.in);
 		UserRegGradle user = new UserRegGradle();
-		System.out.println("Enter the user email id:");
-		email=sc.nextLine();
-		user.EmailCheck(email);
-  }
-}
+		System.out.println("Enter mobile no:");
+		mobile = sc.nextLine();
+		user.MobileCheck(mobile);
 
+	}
+}
