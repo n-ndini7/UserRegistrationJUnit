8dbc486d56bd14fdcf8f0e369a0822be57d31001
n-ndini7 <hp@RJs-PC>
2020-09-30 00:30:45 +0530

    [nandini] added master - welcome message

---
+++ src/main/java/com/capgemini/UserRegGradle.java
@@ -0,0 +1,8 @@
+package com.capgemini;
+
+//Master branch - welcome message
+public class UserRegGradle {
+	public static void main(String[] args) {
+		System.out.println("Welcome to User Registration credentials validation program!");
+	}
+}
