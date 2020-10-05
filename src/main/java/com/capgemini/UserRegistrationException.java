725466ec99af0e7c515a9a67fd4009d4ddb8067b
n-ndini7 <hp@RJs-PC>
2020-10-05 18:28:25 +0530

    [nandini] Refactored UC13 - Using Lambda Expression

--- src/main/java/com/capgemini/UserRegistrationException.java
+++ src/main/java/com/capgemini/UserRegistrationException.java
@@ -2,7 +2,14 @@ package com.capgemini;
 
 public class UserRegistrationException extends Exception {
 	// custom exception class for invalid user entry
-	public UserRegistrationException(String message) {
+	enum ExceptionType {
+		INVALID_FIRST_NAME, INVALID_LAST_NAME, INVALID_EMAIL, INVALID_MOBILE, INVALID_PASSWORD;
+	}
+
+	ExceptionType type;
+
+	public UserRegistrationException(ExceptionType type, String message) {
 		super(message);
+		this.type = type;
 	}
 }
--- src/test/java/com/capgemini/UserRegTest.java
+++ src/test/java/com/capgemini/UserRegTest.java
@@ -6,236 +6,61 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class UserRegTest {
-
 	@Test
-	public void givenFirstName_WhenProper_ShouldReturnTrue() {
-		UserRegGradle user = new UserRegGradle();
+	public void givenAllDetails_WhenAllProper_ShouldReturnTrue() {
 		try {
-			boolean result = user.FirstNameCheck("Nandini");
-			assertEquals(true, result);
+			boolean isValid = UserRegGradle.validate("Nandini", "Gupta", "rng@123.com", "91 9876543210", "Abc@1234t");
+			assertEquals(true, isValid);
 		} catch (UserRegistrationException e) {
-			Assert.assertEquals("Invalid entry of first name!! please re-try!", e.getMessage());
+			e.printStackTrace();
 		}
 	}
 
 	@Test
-	public void givenFirstName_WhenShort_ShouldReturnFalse() {
-		UserRegGradle user = new UserRegGradle();
+	public void givenAllDetails_WhenFirstNameImproper_ShouldThrowCustomException_ofTypeInvalidFirstName() {
 		try {
-			boolean result = user.FirstNameCheck("Nan");
-			assertEquals(true, result);
+			UserRegGradle.validate("N@ndini", "Gupta", "rng@123.com", "91 9876543210", "Abc@1234t");
 		} catch (UserRegistrationException e) {
-			Assert.assertEquals("Invalid entry of first name !! please re-try!", e.getMessage());
+			assertEquals(UserRegistrationException.ExceptionType.INVALID_FIRST_NAME, e.type);
 		}
 	}
 
 	@Test
-	public void givenFirstName_WhenNotCaps_ShouldReturnFalse() {
-		UserRegGradle user = new UserRegGradle();
+	public void givenAllDetails_WhenLastNameImproper_ShouldThrowCustomException_ofTypeInvalidLastName() {
 		try {
-
-			boolean result = user.FirstNameCheck("nandini");
-			assertEquals(false, result);
+			UserRegGradle.validate("Nandini", "Gu", "rng@123.com", "91 9876543210", "Abc@1234t");
 		} catch (UserRegistrationException e) {
-			Assert.assertEquals("Invalid entry of first name !! please re-try!", e.getMessage());
+			assertEquals(UserRegistrationException.ExceptionType.INVALID_LAST_NAME, e.type);
 		}
 	}
 
 	@Test
-	public void givenFirstName_WhenSpecialChar_ShouldReturnFalse() {
-		UserRegGradle user = new UserRegGradle();
+	public void givenAllDetails_WhenEmailImproper_ShouldThrowCustomException_ofTypeInvalidEmail() {
 		try {
-
-			boolean result = user.FirstNameCheck("Nandini@@");
-			assertEquals(false, result);
+			UserRegGradle.validate("Nandini", "Gupta", "rng@", "91 9876543210", "Abc@1234t");
 		} catch (UserRegistrationException e) {
-			Assert.assertEquals("Invalid entry of first name !! please re-try!", e.getMessage());
-		}
-	}
-	// Tests for first name
-
-	@Test
-	public void givenLastName_WhenProper_ShouldReturnTrue() {
-		UserRegGradle user = new UserRegGradle();
-		try {
-			boolean result = user.LastNameCheck("Gupta");
-			assertEquals(true, result);
-		} catch (UserRegistrationException e) {
-			Assert.assertEquals("Invalid entry of last name!! please re-try!", e.getMessage());
+			assertEquals(UserRegistrationException.ExceptionType.INVALID_EMAIL, e.type);
 		}
 	}
 
 	@Test
-	public void givenLastName_WhenShort_ShouldReturnFalse() {
-		UserRegGradle user = new UserRegGradle();
+	public void givenAllDetails_WhenMobileImproper_ShouldThrowCustomException_ofTypeInvalidMobile() {
 		try {
-			boolean result = user.LastNameCheck("Gup");
-			assertEquals(true, result);
+			UserRegGradle.validate("Nandini", "Gupta", "rng@gmail.com", "91 98765", "Abc@1234t");
 		} catch (UserRegistrationException e) {
-			Assert.assertEquals("Invalid entry of last name !! please re-try!", e.getMessage());
+			assertEquals(UserRegistrationException.ExceptionType.INVALID_MOBILE, e.type);
 		}
 	}
 
 	@Test
-	public void givenLastName_WhenNotCaps_ShouldReturnFalse() {
-		UserRegGradle user = new UserRegGradle();
+	public void givenAllDetails_WhenPasswordImproper_ShouldThrowCustomException_ofTypeInvalidPassword() {
 		try {
-			boolean result = user.LastNameCheck("gupta");
-			assertEquals(false, result);
+			UserRegGradle.validate("Nandini", "Gupta", "rng@gmail.com", "91 9876543210", "Abc");
 		} catch (UserRegistrationException e) {
-			Assert.assertEquals("Invalid entry of last name!! please re-try!", e.getMessage());
+			assertEquals(UserRegistrationException.ExceptionType.INVALID_PASSWORD, e.type);
 		}
 	}
 
-	@Test
-	public void givenLastName_WhenSpecialChar_ShouldReturnFalse() {
-		UserRegGradle user = new UserRegGradle();
-		try {
-			boolean result = user.LastNameCheck("Gupta@@");
-			assertEquals(false, result);
-		} catch (UserRegistrationException e) {
-			Assert.assertEquals("Invalid entry of last name!! please re-try!", e.getMessage());
-		}
-	}
-	// Tests for last name
-
-	@Test
-	public void givenMobileNumber_WhenProper_ShouldReturnTrue() {
-		UserRegGradle user = new UserRegGradle();
-		try {
-			boolean result = user.MobileCheck("91 8312345567");
-			assertEquals(true, result);
-		} catch (UserRegistrationException e) {
-			Assert.assertEquals("Invalid entry of mobile no.!! please re-try!", e.getMessage());
-		}
-	}
-
-	@Test
-	public void givenMobileNumber_WhenCodeMoreThanTwoDigits_ShouldReturnTrue() {
-		UserRegGradle user = new UserRegGradle();
-		try {
-			boolean result = user.MobileCheck("124 8312345567");
-			assertEquals(true, result);
-		} catch (UserRegistrationException e) {
-			Assert.assertEquals("Invalid entry of mobile no.!! please re-try!", e.getMessage());
-		}
-	}
-
-	@Test
-	public void givenMobileNumber_WhenHasAnyNonNumeric_ShouldReturnFalse() {
-		UserRegGradle user = new UserRegGradle();
-
-		try {
-			boolean result = user.MobileCheck("91 831234@567");
-			assertEquals(false, result);
-		} catch (UserRegistrationException e) {
-			Assert.assertEquals("Invalid entry of mobile no.!! please re-try!", e.getMessage());
-		}
-	}
-
-	@Test
-	public void givenMobileNumber_WhenNotTenDigits_ShouldReturnFalse() {
-		UserRegGradle user = new UserRegGradle();
-
-		try {
-			boolean result = user.MobileCheck("91 83123455");
-			assertEquals(false, result);
-		} catch (UserRegistrationException e) {
-			Assert.assertEquals("Invalid entry of mobile no.!! please re-try!", e.getMessage());
-		}
-	}
-	// Tests for mobile number
-
-	@Test
-	public void givenEmail_WhenProper_ShouldReturnTrue() {
-		UserRegGradle user = new UserRegGradle();
-
-		try {
-			boolean result = user.EmailCheck("abc@yahoo.com");
-			assertEquals(true, result);
-		} catch (UserRegistrationException e) {
-			Assert.assertEquals("Invalid entry of Email ID!! please re-try!", e.getMessage());
-		}
-	}
-
-	@Test
-	public void givenEmail_ProperSecondLevelDomain_ShouldReturnTrue() {
-		UserRegGradle user = new UserRegGradle();
-		try {
-			boolean result = user.EmailCheck("abc.100@abc.com.au");
-			assertEquals(true, result);
-		} catch (UserRegistrationException e) {
-			Assert.assertEquals("Invalid entry of Email ID!! please re-try!", e.getMessage());
-		}
-	}
-
-	@Test
-	public void givenEmail_InvalidTLD_ShouldReturnFalse() {
-		UserRegGradle user = new UserRegGradle();
-		try {
-			boolean result = user.EmailCheck("abc@123.c");
-			assertEquals(false, result);
-		} catch (UserRegistrationException e) {
-			Assert.assertEquals("Invalid entry of Email ID!! please re-try!", e.getMessage());
-		}
-	}
-
-	@Test
-	public void givenEmail_HavingMultipleTLD_ShouldReturnFalse() {
-		UserRegGradle user = new UserRegGradle();
-		try {
-			boolean result = user.EmailCheck("abc@gmail.com.aa.in");
-			assertEquals(false, result);
-		} catch (UserRegistrationException e) {
-			Assert.assertEquals("Invalid entry of Email ID!! please re-try!", e.getMessage());
-		}
-	}
-	// Tests for Email ID
-
-	@Test
-	public void givenPassword_WhenProper_ShouldReturnTrue() {
-		UserRegGradle user = new UserRegGradle();
-		try {
-			boolean result = user.PasswordCheck("aBcd123@m");
-			assertEquals(true, result);
-		} catch (UserRegistrationException e) {
-			Assert.assertEquals("Invalid entry of Password!! please re-try!", e.getMessage());
-		}
-	}
-
-	@Test
-	public void givenPassword_WhenProperCase2_ShouldReturnTrue() {
-		UserRegGradle user = new UserRegGradle();
-
-		try {
-			boolean result = user.PasswordCheck("@ABC12345");
-			assertEquals(true, result);
-		} catch (UserRegistrationException e) {
-			Assert.assertEquals("Invalid entry of Password!! please re-try!", e.getMessage());
-		}
-	}
-
-	@Test
-	public void givenPassword_WhenNoUpperCase_ShouldReturnFalse() {
-		UserRegGradle user = new UserRegGradle();
-		try {
-			boolean result = user.PasswordCheck("ball@1234");
-			assertEquals(false, result);
-		} catch (UserRegistrationException e) {
-			Assert.assertEquals("Invalid entry of Password!! please re-try!", e.getMessage());
-		}
-	}
-
-	@Test
-	public void givenPassword_WhenNoSpecialChar_ShouldReturnFalse() {
-		UserRegGradle user = new UserRegGradle();
-		try {
-			boolean result = user.PasswordCheck("Apple98765");
-			assertEquals(false, result);
-		} catch (UserRegistrationException e) {
-			Assert.assertEquals("Invalid entry of Password!! please re-try!", e.getMessage());
-		}
-	}
 	// Tests for password
 
 }
